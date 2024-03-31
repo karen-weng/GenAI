c5c3a62f458f0009a0b250f6d6b7780dc69400ce
Angel <angelchen1929@gmail.com>
2024-03-30 21:26:21 -0400

    first bad draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import './App.css';
-import HomePage from './components/index.js';
+import React, { useState } from 'react'
 
 function App() {
+  const [state, setState] = useState('')
+
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+
+        <div class="intro">
+          <br />
+          <h1>GuildAI</h1>
+          <p>GuideAI is a program designed to guide the user step-by-step
+            through a tutorial prompted by the user.</p>
+          <p>To get started, enter an activity you want GuideAI to guide you through.</p>
+          <br />
+        </div>
+
+        <br />
+        <br />
+
+        <div class="userInput">
+          <br />
+          <br />
+          <form>
+            <label htmlFor={'my-input'}>Enter prompt: </label>
+            <br />
+            <br />
+            <textarea rows="4" cols="50"
+              id={'my-input'}
+              type={'text'}
+              class="input-box"
+              value={state}
+              placeholder={'How to...'}
+              onChange={event => {
+                setState(event.target.value)
+              }}
+            />
+            <br />
+            <br />
+            <button onclick="myFunction()"> Submit </button>
+            <br />
+            <br />
+            {/* You entered: {state} */}
+          </form>
+        </div>
+        
     </div>
-  );
+
+  )
 }
+
 
 export default App;
---
+++ src/Components/userText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function userText() {
+  return (
+    <div>userText</div>
+  )
+}
+
+export default userText
